0510d06ab8f326c013532a06d07df5a13eb985f0
GoodGopnik <https://github.com/GoodGopnik>
2021-03-31 19:41:36 +0300

    Сделал второй блок с адаптивом

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
-$(function(){
-
-
-
-  
-});
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+  });
