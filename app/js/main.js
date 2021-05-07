1a1887e7a5642e44031fb7e853d80c0257e143f7
GoodGopnik <https://github.com/GoodGopnik>
2021-05-07 16:34:40 +0300

    Сделал все фички которые планировал

--- app/js/main.js
+++ app/js/main.js
@@ -30,6 +30,29 @@ const modalMessage = () => {
     modalPopup.style.display = 'block'
   })
 }
+
+const clickTarif = () => {
+  let btnMonth = document.querySelector('.btn__month');
+  let btnYears = document.querySelector ('.btn__years');
+  let tariffPriceBlock1 = document.querySelector ('.tariff__price__block1');
+  let tariffPriceBlock2 = document.querySelector ('.tariff__price__block2');
+  let tariffPriceBlock3 = document.querySelector ('.tariff__price__block3');
+  let tariffPriceBlock4 = document.querySelector ('.tariff__price__block4');
+
+      btnMonth.addEventListener('click', () => {
+        tariffPriceBlock1.textContent = '$0 month'
+        tariffPriceBlock2.textContent = '$5 month'
+        tariffPriceBlock3.textContent = '$9 month'
+        tariffPriceBlock4.textContent = '$12 month'
+      })
+      btnYears.addEventListener('click', () => {
+        tariffPriceBlock1.textContent = '$0 year'
+        tariffPriceBlock2.textContent = '$10 year'
+        tariffPriceBlock3.textContent = '$18 year'
+        tariffPriceBlock4.textContent = '$24 year'
+      })
+}
+
+clickTarif()
 modalMessage()
 modalWatch()
-
